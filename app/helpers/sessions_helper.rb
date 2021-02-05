d639d8e2e5123b1c3ced2836469545bf99468f12
horizon <tsrockt@yahoo.co.jp>
2021-02-05 15:29:00 +0000

    Finish full-fledged-login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,10 +2,26 @@ module SessionsHelper
     
     # 引数に渡されたユーザーオブジェクトでログイン。
     def log_in(user)
-       session[:user_id] = user.id 
+       session[:user_id] = user.id
+       # debugger
+    end
+    
+    # 永続的にセッションを記憶します(Userモデルを参照)
+    def remember(user)
+        user.remember
+        cookies.permanent.signed[:user_id] = user.id
+        cookies.permanent[:remember_token] = user.remember_token
+    end
+    
+    # 永続的セッションを破棄します
+    def forget(user)
+        user.forget # Userモデル参照
+        cookies.delete(:user_id)
+        cookies.delete(:remember_token)
     end
     
     def log_out
+        forget(current_user)
         session.delete(:user_id)
         @current_user = nil
     end
@@ -17,10 +33,18 @@ module SessionsHelper
 #           @current_user
 #           end
 #       end
-#   end ↓ 省略形
+#   end ↓ 省略形（セッションログイン機能の内容）
+    # 一時的セッションにいるユーザーを返す
+    # それ以外の場合はcookiesに対応するユーザーを返す。
     def current_user
-        if session[:user_id]
-            @current_user = @current_user || User.find_by(id: session[:user_id])
+        if (user_id = session[:user_id])
+            @current_user = @current_user || User.find_by(id: user_id)
+        elsif (user_id = cookies.signed[:user_id])
+            user = User.find_by(id: user_id)
+            if user && user.authenticated?(cookies[:remember_token])
+                log_in user
+                @current_user = user
+            end
         end
     end
     # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
     has_secure_password
     validates :password, presence: true, length: {minimum: 6}
     
-    # 渡された文字列のハッシュ地を返す。
+    # 渡された文字列のハッシュ値を返す。
     def User.digest(string)
         cost =
             if ActiveModel::SecurePassword.min_cost
@@ -23,7 +23,7 @@ class User < ApplicationRecord
     
     # ランダムなトークンを返す。
     def User.new_token
-        SecureRandom.urlsage_base64
+        SecureRandom.urlsafe_base64
     end
     
     # 永続セッションのためハッシュ化したトークンを
@@ -32,5 +32,17 @@ class User < ApplicationRecord
     def remember
         self.remember_token = User.new_token
         update_attribute(:remember_digest, User.digest(remember_token))
+    end
+    
+    # トークンがダイジェストと一致すればtrueを返します。
+    def authenticated?(remember_token)
+        # ダイジェストが存在しない場合はfalseを返して終了します。
+        return false if remember_digest.nil?
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+    
+    # ユーザーのログイン情報を破棄します。
+    def forget
+        update_attribute(:remember_digest, nil)
     end
 end
